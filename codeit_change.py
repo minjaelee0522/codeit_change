bff4d8b433cc7ba87912415acab8690cfe87a1a0
minjae <32402820+kkrlee@users.noreply.github.com>
2019-03-15 16:19:58 -0400

    fix code

--- codeit_change.py
+++ codeit_change.py
@@ -1,27 +1,16 @@
 def calculate_change(payment, cost):
-    fifty_thousand = 50000
-    ten_thousand = 10000
-    five_thousand = 5000
-    one_thousand = 1000
-
     change = payment - cost
-    fifty_thousand_count = int(change / 50000)
 
-    change = change % 50000
-    ten_thousand_count = int(change / 10000)
+    fifty_thousand = change // 50000
+    ten_thousand = (change - fifty_thousand * 50000) // 10000
+    five_thousand = (change - fifty_thousand * 50000 - ten_thousand * 10000) // 5000
+    one_thousand = (change - fifty_thousand * 50000 - ten_thousand * 10000 - five_thousand * 5000) // 1000
 
-    change = change % 10000
-    five_thousand_count = int(change / 5000)
-
-    change = change % 5000
-    one_thousand_count = int(change / 1000)
-
-    print("50000원 지폐: %d장" % (fifty_thousand_count))
-    print("10000원 지폐: %d장" % (ten_thousand_count))
-    print("5000원 지폐: %d장" % (five_thousand_count))
-    print("1000원 지폐: %d장" % (one_thousand_count))
-
+    print("50000원 지폐 : %d장" % (fifty_thousand))
+    print("10000원 지폐 : %d장" % (ten_thousand))
+    print("5000원 지폐 : %d장" % (five_thousand))
+    print("1000원 지폐 : %d장" % (one_thousand))
 
 calculate_change(100000, 33000)
-
+print()
 calculate_change(500000, 378000)
